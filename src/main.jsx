0ede94b26e7746844b1267f39f8d63e71e7f8d3a
marieastra <korikdesign@gmail.com>
2025-03-04 16:42:07 +0500

    git init

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // если есть стили
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
